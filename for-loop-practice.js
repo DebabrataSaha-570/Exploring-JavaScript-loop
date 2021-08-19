6300cbfb66cbd65c4edcbe3d1deaa45e0b6a2fc1
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-20 02:07:00 +0600

    learned structure of for loop and how to get odd number and even number using for loop

---
+++ for-loop-practice.js
@@ -0,0 +1,23 @@
+// while loop
+
+var kanDhor = 0;
+
+while (kanDhor <= 7) {
+    console.log('ami ar beyadopi korbo na boss');
+    kanDhor++;
+}
+
+// for loop 
+for (kanDhor = 0; kanDhor <= 10; kanDhor++) {
+    console.log('ami ar beyadopi korbo na boos')
+}
+
+// odd number 
+for (i = 1; i <= 20; i += 2) {
+    console.log(i)
+}
+
+// even number 
+for (i = 0; i <= 20; i += 2) {
+    console.log(i)
+}
