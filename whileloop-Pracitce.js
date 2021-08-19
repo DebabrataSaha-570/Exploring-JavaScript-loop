bc9b172c4abb8c15df2cb1beeceacba67e419a69
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-20 00:12:29 +0600

    learned basic of while loop and how to debug while loop on vs code

---
+++ whileloop-Pracitce.js
@@ -0,0 +1,14 @@
+var kaneDhoreUthBosh = 0;
+
+while (kaneDhoreUthBosh <= 10) {
+    console.log('ami ar beyadopi korbo na')
+    kaneDhoreUthBosh++
+}
+
+
+var msgToJan = 0;
+
+while (msgToJan <= 50) {
+    console.log('I love you honey, I wont do it anymore!!');
+    msgToJan++
+}
