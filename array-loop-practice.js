3f6df9ad2616a173ac2b2eda38f601d59cb1c31e
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-20 02:50:40 +0600

    learned how to for loop through an array

---
+++ array-loop-practice.js
@@ -0,0 +1,11 @@
+var rollNumbers = [1, 4, 5, 6, 8, 12, 14, 15]
+for (i = 0; i < rollNumbers.length; i++) {
+    var element = rollNumbers[i]
+    console.log(element)
+}
+
+var nayikaName = ['katrina', 'dipika', 'illiana', 'anuska']
+for (i = 0; i < nayikaName.length; i++) {
+    var element = nayikaName[i];
+    console.log(element)
+}
